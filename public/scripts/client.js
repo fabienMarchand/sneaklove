b31917aaee0e629c714cad20eacc27d6f3387f3d
Marcha <marchaf@gmail.com>
2020-09-25 14:28:11 +0200

    add client side

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,17 +2,33 @@ import ApiHandler from "./apiHandler.js";
 const stylesApi = new ApiHandler("/api/styles");
 
 
+editListeners();
 
-const boxCheck = document.querySelectorAll(".checkbox");
 
-boxCheck.forEach(box => 
+function editListeners() {
+  //do something
+  const boxCheck = document.querySelectorAll(".checkbox");
+  const arrBox = [];
+  boxCheck.forEach((box) => {
+   
+    box.onchange = (event) => {
+      if (box.checked) {
+        arrBox.push(event.target.getAttribute("data-tag-id"));
+      } else {
+        arrBox.filter(function(e) { return e !==  event.target.getAttribute("data-tag-id")})
+      }
+      console.log(arrBox);
     
-
-
-
-    
-    console.log(box.getAttribute("data-tag-id"))
-);
-
+      // stylesApi
+      // .deleteOne(id) // Make the call to delete an element
+      // .then((apiResponse) => {
+      //   parentRow.remove(); // remove the row that has just been clicked
+      // })
+      // .catch((apiError) => {
+      //   cons
+    };
+  
+  });
+}
 
 console.log("hello");
